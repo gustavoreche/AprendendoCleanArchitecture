24934acb727be167450b9444dd0ca651ab97c2cf
gustavoreche <gustavodreche@msn.com>
2020-10-02 17:09:19 -0300

    Corrige retorno da classe Cpf

--- src/main/java/empresaA/Cpf.java
+++ src/main/java/empresaA/Cpf.java
@@ -3,15 +3,22 @@ package empresaA;
 public class Cpf {
 
 	private static final String VALIDA_CPF = "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$";
+	
+	private String numero;
 
-	public String insere(String numero) {
+	public Cpf insere(String numero) {
 		if(numero == null || !ehValido(numero))
 			throw new CpfInvalidoException();
-		return numero;
+		this.numero = numero;
+		return this;
 	}
 
 	protected boolean ehValido(String numero) {
 		return new ValidadorPorExpressaoRegular(VALIDA_CPF).ehValido(numero);
+	}
+	
+	public String getNumero() {
+		return numero;
 	}
 
 }
