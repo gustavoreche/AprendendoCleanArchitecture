f1ca9ded059c89bd8072aff455d0d987a66587d7
gustavoreche <gustavodreche@msn.com>
2020-10-02 17:18:49 -0300

    Cria VO Email

---
+++ src/main/java/empresaA/Email.java
@@ -0,0 +1,24 @@
+package empresaA;
+
+public class Email {
+	
+	private static final String VALIDA_EMAIL = "^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+	
+	private String endereco;
+	
+	public Email insere(String endereco) {
+		if(endereco == null || !ehValido(endereco))
+			throw new EmailInvalidoException();
+		this.endereco = endereco;
+		return this;
+	}
+
+	protected boolean ehValido(String numero) {
+		return new ValidadorPorExpressaoRegular(VALIDA_EMAIL).ehValido(numero);
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	
+}
