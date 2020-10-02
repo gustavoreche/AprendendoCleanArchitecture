c793ec8750d266b6f92ae8ceeb719c3d2190266d
gustavoreche <gustavodreche@msn.com>
2020-10-02 17:57:05 -0300

    Cria atributo lista de Telefone, e metodo para adicionar telefone

--- src/main/java/empresaA/Funcionario.java
+++ src/main/java/empresaA/Funcionario.java
@@ -1,9 +1,16 @@
 package empresaA;
 
+import java.util.List;
+
 public class Funcionario {
 	
 	private Cpf cpf;
 	private String nome;
 	private Email email;
+	private List<Telefone> telefones;
+	
+	public void adicionaTelefone(String ddd, String numero) {
+		this.telefones.add(new Telefone().insere(ddd, numero));
+	}
 
 }
