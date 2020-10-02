ad0dc85881056f111ec1d35bf6f1afae6a8780bd
gustavoreche <gustavodreche@msn.com>
2020-10-02 17:57:18 -0300

    Cria VO Telefone

---
+++ src/main/java/empresaA/Telefone.java
@@ -0,0 +1,31 @@
+package empresaA;
+
+public class Telefone {
+	
+	private static final String VALIDA_DDD = "\\d{3}";
+	private static final String VALIDA_TELEFONE = "\\d{4,5}-\\d{4}";
+	
+	private String ddd;
+	private String numero;
+
+	public Telefone insere(String ddd, String numero) {
+		if(ddd == null || numero == null || !ehValido(ddd, numero))
+			throw new TelefoneInvalidoException();
+		this.numero = numero;
+		this.ddd = ddd;
+		return this;
+	}
+
+	protected boolean ehValido(String ddd, String numero) {
+		return new ValidadorPorExpressaoRegular(VALIDA_DDD).ehValido(ddd) && new ValidadorPorExpressaoRegular(VALIDA_TELEFONE).ehValido(numero);
+	}
+	
+	public String getDdd() {
+		return ddd;
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+
+}
