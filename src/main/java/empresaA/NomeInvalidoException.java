5f6906f88ab9f8f018be0fb961fc19e246110b70
gustavoreche <gustavodreche@msn.com>
2020-10-03 12:13:38 -0300

    Cria exception caso nome esteja vazio ou nulo

---
+++ src/main/java/empresaA/NomeInvalidoException.java
@@ -0,0 +1,13 @@
+package empresaA;
+
+public class NomeInvalidoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static final String NOME_INVALIDO = "Nome inválido!";
+	
+	public NomeInvalidoException() {
+		super(NOME_INVALIDO);
+	}
+
+}
